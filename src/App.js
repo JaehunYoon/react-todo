5e88caec6cddc6de4d48ba74b7454426ecbbd6c8
JaehunYoon <goodasd123@gmail.com>
2019-06-20 17:07:13 +0900

    Form Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import TodoTemplate from './components/TodoTemplate';
+import Form from "./components/Form";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <TodoTemplate>
+        <TodoTemplate form={<Form/>}>
           Test
         </TodoTemplate>
       </div>
