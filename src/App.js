2718bf35d9a121e192ec02efd43bba7489a161e5
JaehunYoon <goodasd123@gmail.com>
2019-06-22 10:49:35 +0900

    삭제 기능 구현 완료

--- src/App.js
+++ src/App.js
@@ -55,13 +55,21 @@ class App extends Component {
     });
   }
 
+  handleRemove = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.filter(todo => todo.id !== id)
+    })
+  }
+
   render() {
     const { input, todos } = this.state;
     const {
       handleChange,
       handleCreate,
       handleKeyPress,
-      handleToggle
+      handleToggle,
+      handleRemove
     } = this;
 
     return (
@@ -73,7 +81,7 @@ class App extends Component {
           onCreate={handleCreate}
         />
       )}>
-        <TodoItemList todos={todos} onToggle={handleToggle}/>
+        <TodoItemList todos={todos} onToggle={handleToggle} onRemove={handleRemove}/>
       </TodoTemplate>
     )
   }
